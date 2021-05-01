3ef51d2c72fe2aa89f7f7ee73639e5c68d5ea7d3
Terry Moreland <tsmoreland@live.com>
2021-05-01 11:55:58 +0100

    scaffolded p roject from start.spring.io

---
+++ springJDBC/src/main/java/moreland/spring/sample/mysqldemo/MysqldemoApplication.java
@@ -0,0 +1,13 @@
+package moreland.spring.sample.mysqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqldemoApplication.class, args);
+	}
+
+}
---
+++ springJDBC/src/test/java/moreland/spring/sample/mysqldemo/MysqldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package moreland.spring.sample.mysqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

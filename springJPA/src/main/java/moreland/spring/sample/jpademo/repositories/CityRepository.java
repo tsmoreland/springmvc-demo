3235ccc3b017a05fe95588dbf17162de3be7bb23
Terry Moreland <tsmoreland@live.com>
2021-05-20 02:11:05 +0100

    updated city repository - added method to find by city name and province id, helpful to determine if city already exists

--- springJPA/src/main/java/moreland/spring/sample/jpademo/repositories/CityRepository.java
+++ springJPA/src/main/java/moreland/spring/sample/jpademo/repositories/CityRepository.java
@@ -20,6 +20,8 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CityRepository extends JpaRepository<City, Long>, CityReadRepository {
     
@@ -27,4 +29,10 @@ public interface CityRepository extends JpaRepository<City, Long>, CityReadRepos
     Page<City> findByNameContains(String name, Pageable pageable);
     Page<City> findByProvinceNameInOrderByNameAsc(List<String> provinceNames, Pageable pageable);
     Page<City> findByProvinceNameInOrderByNameDesc(List<String> provinceNames, Pageable pageable);
+
+    @Query("""
+        select distinct c from City c
+        where c.name = :name and c.provinceId = :provinceId
+    """)
+    Optional<City> findByNameAndProvinceId(@Param("name") String name, @Param("provinceId") Long provinceId);
 }

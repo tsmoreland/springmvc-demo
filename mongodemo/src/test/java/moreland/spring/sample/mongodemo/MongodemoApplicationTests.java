a5b02b42c68b327d872bff019e5a1bddc99f43da
Terry Moreland <tsmoreland@live.com>
2021-05-23 22:14:12 +0100

    mongodb demo initial commit

---
+++ mongodemo/src/test/java/moreland/spring/sample/mongodemo/MongodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package moreland.spring.sample.mongodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

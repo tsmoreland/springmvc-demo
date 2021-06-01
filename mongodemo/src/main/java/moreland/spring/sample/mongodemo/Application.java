ffbb8a87a19e79d5d0446b9bb420b4f29a442f24
Terry Moreland <tsmoreland@live.com>
2021-06-01 21:46:12 +0100

    rename application

--- mongodemo/src/main/java/moreland/spring/sample/mongodemo/Application.java
+++ mongodemo/src/main/java/moreland/spring/sample/mongodemo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MongodemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MongodemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }

aa0940a32ee6330d53a607343c5aba907073c87b
Terry Moreland <tsmoreland@live.com>
2021-06-05 00:28:01 +0100

    some mongo queries/commands

--- mongodemo/src/main/java/moreland/spring/sample/mongodemo/commands/CountryCommands.java
+++ mongodemo/src/main/java/moreland/spring/sample/mongodemo/commands/CountryCommands.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import moreland.spring.sample.mongodemo.domain.Country;
@@ -19,11 +21,16 @@ public class CountryCommands {
     }
 
     public Country Create(Country item) {
-        Guard.Against.ArgumentNull(item, "item");
+        Guard.Against.argumentNull(item, "item");
         return mongoTemplate.insert(item);
     }
 
     public List<Country> Create(Country... items) {
+
+        if (Arrays.stream(items).anyMatch(i -> i == null)) {
+            throw new IllegalArgumentException("items contains null item");
+        }
+
         // TODO: validate that no item in items is null
         var asList = Arrays.asList(items);
         return mongoTemplate.insert(asList);
@@ -33,4 +40,14 @@ public class CountryCommands {
         mongoTemplate.save(item);
     }
 
+    public void Delete(Country item) {
+        mongoTemplate.remove(item);
+    }
+    public void Delete(String id) {
+        var query = Query.query(Criteria.where("id").is(id));
+        mongoTemplate.findAndRemove(query, Country.class);
+    }
+    public void DeleteAll() {
+        mongoTemplate.dropCollection(Country.class);
+    }
 }

6240297c75428ee4bda082548b70acf7838f9e18
Terry Moreland <tsmoreland@live.com>
2021-06-05 00:28:01 +0100

    some mongo queries/commands

--- mongodemo/src/main/java/moreland/spring/sample/mongodemo/queries/CountryQueries.java
+++ mongodemo/src/main/java/moreland/spring/sample/mongodemo/queries/CountryQueries.java
@@ -15,25 +15,25 @@ public class CountryQueries implements SpecificationQueries<Country> {
     private final MongoTemplate mongoTemplate;
 
     public CountryQueries(MongoTemplate mongoTemplate) {
-        Guard.Against.ArgumentNull(mongoTemplate, "mongoTemplate");
+        Guard.Against.argumentNull(mongoTemplate, "mongoTemplate");
         this.mongoTemplate = mongoTemplate;
     }
 
     @Override
     public List<Country> findAll(Specification specification) {
-        Guard.Against.ArgumentNull(specification, "specification");
+        Guard.Against.argumentNull(specification, "specification");
         return mongoTemplate.find(specification.query(), Country.class);
     }
 
     @Override
     public Country findOne(Specification specification) {
-        Guard.Against.ArgumentNull(specification, "specification");
+        Guard.Against.argumentNull(specification, "specification");
         return mongoTemplate.findOne(specification.query(), Country.class);
     }
 
     @Override
     public Long count(Specification specification) {
-        Guard.Against.ArgumentNull(specification, "specification");
+        Guard.Against.argumentNull(specification, "specification");
         return mongoTemplate.count(specification.query(), Country.class);
     }
     

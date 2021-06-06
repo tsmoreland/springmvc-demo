0f8e8f72a50763ff0c6f44f925eb29f2af9bb02b
Terry Moreland <tsmoreland@live.com>
2021-06-06 03:18:17 +0100

    copying guard clause class over

--- mongodemo/src/main/java/moreland/spring/sample/mongodemo/internal/Guard.java
+++ mongodemo/src/main/java/moreland/spring/sample/mongodemo/internal/Guard.java
@@ -1,31 +1,39 @@
 package moreland.spring.sample.mongodemo.internal;
 
+import java.util.Arrays;
+
 public class Guard {
     
     public class Against {
-        public static void ArgumentNull(Object value, String name) {
+        public static <T> void argumentNull(T value, String name) { // Object would be fine, I justed wanted to use a generic somewhere
             if (value == null) {
                 throw new IllegalArgumentException("%s cannot be null".formatted(name));
             }
         }
-        public static void ArgumentNullOrEmpty(String value, String name) {
+        public static void argumentNullOrEmpty(String value, String name) {
             if (value == null || value.isEmpty()) {
                 throw new IllegalArgumentException("%s cannot be null".formatted(name));
             }
         }
-        public static void ArgumentNullOrBlank(String value, String name) {
+        public static void argumentNullOrBlank(String value, String name) {
             if (value == null || value.isBlank()) {
                 throw new IllegalArgumentException("%s cannot be null".formatted(name));
             }
         }
-        public static void ArgumentNegative(int value, String name) {
+        public static void argumentNegative(int value, String name) {
             if (value < 0) {
                 throw new IllegalArgumentException("%s cannot be less than zero".formatted(name));
             }
         }
-        public static void ArgumentNegative(long value, String name) {
+        public static void argumentNegative(long value, String name) {
             if (value < 0) {
                 throw new IllegalArgumentException("%s cannot be less than zero".formatted(name));
+            }
+        }
+
+        public static void argumentContainsNullItem(String name, Object... source) {
+            if (Arrays.stream(source).anyMatch(i -> i == null)) {
+               throw new IllegalArgumentException("%s contains at least one null element".formatted(name)); 
             }
         }
     }

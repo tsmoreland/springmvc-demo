83ca9689482579fa4a3ea938b173a79f0cd5d526
Terry Moreland <tsmoreland@live.com>
2021-06-11 02:07:50 +0100

    renamed application test file - isn't seen as rename, must've modified it too much

---
+++ mongodemo/src/test/java/moreland/spring/sample/mongodemo/ApplicationTests.java
@@ -0,0 +1,15 @@
+package moreland.spring.sample.mongodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// no assert required, this is in part a place holder and in part just a test to ensure the context loads
+		// if we even get this far the test passes so we don't need any further checks
+	}
+
+}

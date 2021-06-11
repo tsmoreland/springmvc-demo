fc9de67ba092e6bf079e5895335f20ce6b6a9b94
Terry Moreland <tsmoreland@live.com>
2021-06-12 00:04:09 +0100

    country repository, alternate to command/query approach

--- mongodemo/src/main/java/moreland/spring/sample/mongodemo/repositories/CountryRepository.java
+++ mongodemo/src/main/java/moreland/spring/sample/mongodemo/repositories/CountryRepository.java
@@ -14,8 +14,8 @@ package moreland.spring.sample.mongodemo.repositories;
 
 import java.util.Optional;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 

4fb48bd2b6c7f04dca4d9e63fb1e477e2c92568c
Terry Moreland <tsmoreland@live.com>
2021-12-03 21:53:15 +0000

    removed csharp samples as they have been moved to csharp-misc

--- csharp/HttpsRedirect/HttpsRedirectCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-
-namespace HttpsRedirectCore.Models;
-
-public sealed class ErrorViewModel
-{
-    public string RequestId { get; set; } = string.Empty;
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- csharp/HttpsRedirect/HttpsRedirectNet/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HttpsRedirect.MvcApplication" Language="C#" %>

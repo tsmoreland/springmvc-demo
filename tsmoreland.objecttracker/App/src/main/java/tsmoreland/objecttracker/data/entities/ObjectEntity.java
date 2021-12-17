dab8e49349199935d6f7fa26f0043ed70a33ec15
Terry Moreland <tsmoreland@live.com>
2021-12-17 23:44:50 +0000

    added Table annotation to ObjectEntity

--- tsmoreland.objecttracker/App/src/main/java/tsmoreland/objecttracker/data/entities/ObjectEntity.java
+++ tsmoreland.objecttracker/App/src/main/java/tsmoreland/objecttracker/data/entities/ObjectEntity.java
@@ -16,8 +16,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "Objects")
 public class ObjectEntity {
     
     @Id

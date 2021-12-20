b2c01e805569feb4b6f67183845006cace9895a5
Terry Moreland <tsmoreland@live.com>
2021-12-20 00:26:38 +0000

    untested find method added ObjectRepository

--- tsmoreland.objecttracker/App/src/main/java/tsmoreland/objecttracker/data/repositories/ObjectRepository.java
+++ tsmoreland.objecttracker/App/src/main/java/tsmoreland/objecttracker/data/repositories/ObjectRepository.java
@@ -1,12 +1,19 @@
 package tsmoreland.objecttracker.data.repositories;
 
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import tsmoreland.objecttracker.data.entities.LogEntryEntity;
 import tsmoreland.objecttracker.data.entities.ObjectEntity;
 
 @Repository
 public interface ObjectRepository extends PagingAndSortingRepository<ObjectEntity, Long> {
+    @Query(
+        value = "select l from LogEntity l where l.ObjectEntityId = :id")
+    Page<LogEntryEntity> findLogsByObjectEntityId(Long id, Pageable pageable);
 
 }

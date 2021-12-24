1323989ac1c4bd7b140ed19053a7fd5b233073ec
Terry Moreland <tsmoreland@live.com>
2021-12-24 01:04:42 +0000

    added method to convert integer to Severity

--- tsmoreland.objecttracker/App/src/main/java/tsmoreland/objecttracker/core/models/Severity.java
+++ tsmoreland.objecttracker/App/src/main/java/tsmoreland/objecttracker/core/models/Severity.java
@@ -12,8 +12,28 @@ public enum Severity {
         this.value = value;
     }
 
-    public int asInteger() {
+    public int asInt() {
         return value;
     }
 
+    public Integer asInteger() {
+        return Integer.valueOf(value);
+    }
+
+    /**
+     * converts {@code value} to {@code Severity} if {@code value}
+     * is a valid value in the enum, otherwise {@code Severity.LOW}
+     * @param value integer value to convert
+     * @return converted {@code Severity} or {@code Severity.LOW} if
+     *         {@code value} is invalid
+     */
+    public static Severity FromInteger(Integer value) {
+        return switch (value.intValue()) {
+            case 0 -> Severity.LOW;
+            case 1 -> Severity.MEDIUM;
+            case 2 -> Severity.HIGH;
+            default -> Severity.LOW;
+        };
+
+    }
 }

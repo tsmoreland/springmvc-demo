63a5b10eaba58ac22c96738794ae7d8d8c0b27fd
Terry Moreland <tsmoreland@live.com>
2022-01-24 09:15:49 +0000

    remove commented out error

--- tsmoreland.objecttracker/App/src/main/java/tsmoreland/objecttracker/data/repositories/ObjectRepository.java
+++ tsmoreland.objecttracker/App/src/main/java/tsmoreland/objecttracker/data/repositories/ObjectRepository.java
@@ -33,20 +33,6 @@ public interface ObjectRepository extends PagingAndSortingRepository<ObjectEntit
     """)
     Page<ObjectEntitySummaryView> findAllViews(Pageable pageable);
 
-    /*
-
-Caused by: java.lang.IllegalArgumentException:
-org.hibernate.QueryException: could not resolve property: ObjectEntityId of: tsmoreland.objecttracker.data.entities.LogEntryEntity [    select l from tsmoreland.objecttracker.data.entities.LogEntryEntity l
-    where l.ObjectEntityId = :id
-]
-Caused by: org.hibernate.QueryException:
-could not resolve property: ObjectEntityId of: tsmoreland.objecttracker.data.entities.LogEntryEntity [    select l from tsmoreland.objecttracker.data.entities.LogEntryEntity l
-    where l.ObjectEntityId = :id
-]
-Caused by: org.hibernate.QueryException: could not resolve property: ObjectEntityId of: tsmoreland.objecttracker.data.entities.LogEntryEntity
-    */
-
-
     @Query(value = """
         select l from LogEntryEntity l
         where l.objectEntity.id = :id

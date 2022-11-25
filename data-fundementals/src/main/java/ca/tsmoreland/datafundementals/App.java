190e5f49432d925043b38a92727f05a7f9a0b1fb
Terry Moreland <terry.s.moreland@gmail.com>
2022-11-25 16:45:56 +0000

    adding initial project + docker-compose.yml

---
+++ data-fundementals/src/main/java/ca/tsmoreland/datafundementals/App.java
@@ -0,0 +1,13 @@
+package ca.tsmoreland.datafundementals;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

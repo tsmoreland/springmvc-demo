ef8f3b637a5eef612876bc9abea5b63849263a58
Terry Moreland <terry.s.moreland@gmail.com>
2022-11-26 15:03:43 +0000

    update App to verify find by id

--- data-fundementals/src/main/java/ca/tsmoreland/datafundementals/App.java
+++ data-fundementals/src/main/java/ca/tsmoreland/datafundementals/App.java
@@ -5,6 +5,7 @@ import ca.tsmoreland.datafundementals.infrastructure.repository.Dao;
 import ca.tsmoreland.datafundementals.model.Book;
 
 import java.util.List;
+import java.util.Optional;
 
 public class App {
     public static void main( String[] args ) {
@@ -16,5 +17,7 @@ public class App {
             System.out.println(book);
         }
 
+        Optional<Book> maybeBook = bookDao.findById(1);
+        maybeBook.ifPresent(System.out::println);
     }
 }

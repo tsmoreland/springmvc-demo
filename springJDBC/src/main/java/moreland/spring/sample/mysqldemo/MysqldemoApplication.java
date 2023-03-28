a6075c18b4b4b174c99040c67df4088f116ac624
Terry Moreland <tsmoreland@live.com>
2023-03-28 20:39:36 +0100

    remove unused imports

--- springJDBC/src/main/java/moreland/spring/sample/mysqldemo/MysqldemoApplication.java
+++ springJDBC/src/main/java/moreland/spring/sample/mysqldemo/MysqldemoApplication.java
@@ -1,10 +1,8 @@
 package moreland.spring.sample.mysqldemo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class MysqldemoApplication {
